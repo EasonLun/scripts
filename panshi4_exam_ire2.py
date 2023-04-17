007f0f9674ea28543a12e4e5683ac493b05dfcb1
heason_lyc <yctt123@126.com>
2023-04-17 20:39:37 +0800

    Initial commit

---
+++ panshi4_exam_ire2.py
@@ -0,0 +1,23 @@
+import re
+from urllib.parse import unquote
+
+lines = []
+with open(r'C:\Users\yibo\Desktop\access.log', 'r') as f:
+    raw_lines = f.readlines()
+    for i in raw_lines:
+        if 'key2_is_here' in i:
+            lines.append(i)
+# print(lines)
+
+new_lines = []
+for j in lines:
+    a = unquote(j.split(' ')[6])
+    if '!=' in a:
+        pattern = re.compile(r'!=[0-9]+')
+        key = re.findall(pattern, a)[0]
+        num_key = str(key).replace('!=', '')
+        str_key = chr(int(num_key))
+        print(str_key, end='')
+
+
+
